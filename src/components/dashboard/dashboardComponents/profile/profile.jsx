501771422fcfaf31eaa627a0a2f7da04dea80721
trulyaman25 <trulyaman25@gmail.com>
2024-05-11 16:36:02 +0530

    Minor Changes

--- src/components/dashboard/dashboardComponents/profile/profile.jsx
+++ src/components/dashboard/dashboardComponents/profile/profile.jsx
@@ -1,13 +1,19 @@
 import './profileStyles.css'
-import React from "react";
-import DashboardNavbar from "../dashboardNavbar/dashboardNavbar";
+import React from "react"
+import DashboardNavbar from "../dashboardNavbar/dashboardNavbar"
 
 function Profile() {
     return (
+        <>
             <div className='md_navBar'>
                 <DashboardNavbar />
             </div>
+
+            <div className='md_primaryScreen'>
+                
+            </div>
+        </>
     );
-}
+};
 
 export default Profile;
