a104eb3451d3fecfebd5e4d60c36dd754ce37ebe
Aman Kumar <trulyaman25@gmail.com>
2024-09-28 21:45:04 +0530

    Updated Auth0

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<Auth0Provider
-		domain="dev-ie7ibelj5jg8jr1d.us.auth0.com"
-		clientId="wLaewKrlPf2XuGnco4tv6w5U7QZafABZ"
+		domain="trulyaman25.us.auth0.com"
+    	clientId="uvUqiVwXm0ppLF1Gh3NgBJ8Rirr26eTE"
 		authorizationParams={{
 			redirect_uri: window.location.origin
 		}}
